b386af0574393e1d35cfe0a9b321416411f85ca6
dahun0101 <dahun0101@ajou.ac.kr>
2021-01-31 17:19:40 +0900

    기본 API 예시 작성 TEST 코드 작성

---
+++ src/main/java/exitProject/web/dto/mainResponseDto.java
@@ -0,0 +1,13 @@
+package exitProject.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class mainResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
---
+++ src/test/java/exitProject/web/dto/mainResponseDtoTest.java
@@ -0,0 +1,19 @@
+package exitProject.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class mainResponseDtoTest {
+
+    @Test
+    public void 롬복_테스트() {
+        String name = "Test";
+        int amount = 1000;
+
+        mainResponseDto dto = new mainResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
